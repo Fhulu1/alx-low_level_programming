adb0ff20b3f9e0e801881b61b76e060c0dad0863
Fhulufhelo <fhulu.netshimbupfe@gmail.com>
2023-09-18 09:36:42 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_a_string - print a string in reserves, followed by new line.
+ * @str - a pointer to pring string.
+ *
+ * Return: prints a line in reserve
+ */
+
+void print_rev(char *s)
+{
+
+int i, j, len;
+
+i = 0;
+
+while (s[i] != '\0')
+{
+i++;
+}
+
+len = i;
+
+for (j = len - 1; j >= 0; j--)
+{
+_putchar(s[j]);
+}
+
+_putchar('\n');
+}
