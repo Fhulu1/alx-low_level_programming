81080eb6e77ad6932e63c41aebb5244b4cd69773
Fhulufhelo <fhulu.netshimbupfe@gmail.com>
2023-09-19 08:15:34 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Function will return multiplation of a, b
+ * a - integer
+ * b - integer
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
