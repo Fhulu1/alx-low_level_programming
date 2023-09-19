32ea1685f6d90915e3e68a6a90ed9848d1e1ece1
Fhulufhelo <fhulu.netshimbupfe@gmail.com>
2023-09-19 08:05:49 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * The function will check for single digits
+ * _isdigit - the function to check for single digits
+ * c - integer
+ * Return: function return 1 or 0
+ */
+
+int _isdigit(int c)
+{
+if ((c >= 0) && (c <= 9))
+
+return (1);
+
+return (0);
+}
