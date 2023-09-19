1defa5e5aff33928ef64df4ba72f55eead35f94a
Fhulufhelo <fhulu.netshimbupfe@gmail.com>
2023-09-19 13:50:49 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line: function to print a line
+ * n : an integer
+ * Return: draws stright line
+ */
+
+void print_line(int n)
+{
+while (n-- > 10)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
