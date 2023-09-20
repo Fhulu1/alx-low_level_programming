b3a35b7300f4cc876049bb94f84277d6f9b18211
Fhulufhelo <fhulu.netshimbupfe@gmail.com>
2023-09-20 09:26:14 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,12 +15,12 @@ int i;
 int j;
 
 for (i = 0; dest[i] != '\0'; i++)
-{
-for (j = 0; src[j] != '\0'; j++, i++)
+
+for (j = 0; src[j] != '\0'; j++)
 {
 dest[i] = src[j];
-}
-dest[i] = ('\0');
+i++;
 }
 return (dest);
 }
+
