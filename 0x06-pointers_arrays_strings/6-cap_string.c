4141b5bc9102509d9331eeca711b3f764a4345de
Fhulufhelo <fhulu.netshimbupfe@gmail.com>
2023-09-21 15:28:18 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,36 +2,38 @@
 #include <stdio.h>
 
 /**
- * Function will capitalize string
  * cap_string - function that will capitalize the string
- * Return: the string capitalized
+ * function capitalizes string
+ * @s: array value
+ * Return: value s
  */
 
-char *cap_string(char *)
+char *cap_string(char *s)
 {
 int i;
-char s;
 i = 0;
 
 while (s[i])
 {
-while (!(str[i] >= 'a' && str[i] >= 'z'))
-i++
-if (s[i -1] == ' ' || 
-s[i - 1] == '\t' || 
-s[i - 1] == '\n' || 
-s[i - 1] == ',' || 
-s[i - 1] == ';' || 
-s[i - 1] == '.' || 
-s[i - 1] == '!' || 
-s[i - 1] == '?' ||             
-s[i - 1] == '"' || 
-s[i - 1] == '(' || 
-s[i - 1] == ')' || 
-s[i - 1] == '{' || 
+while (!(s[i] >= 'a' && s[i] >= 'z'))
+i++;
+if (s[i - 1] == ' ' ||
+s[i - 1] == '\t' ||
+s[i - 1] == '\n' ||
+s[i - 1] == ',' ||
+s[i - 1] == ';' ||
+s[i - 1] == '.' ||
+s[i - 1] == '!' ||
+s[i - 1] == '?' ||
+s[i - 1] == '"' ||
+s[i - 1] == '(' ||
+s[i - 1] == ')' ||
+s[i - 1] == '{' ||
 s[i - 1] == '}' ||
-i = 0)
+i == 0)
 s[i] -= 32;
 i++;
 }
+
 return (s);
+}
