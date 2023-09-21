cc0eb862f6ca431e67f595cac4316edb9605b905
Fhulufhelo <fhulu.netshimbupfe@gmail.com>
2023-09-21 12:19:48 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,6 +7,17 @@
  * Return: The string in uppercase
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *s)
 {
+int i;
 
+while (*(s + i) != '\0')
+{
+if (*(s + i) >= 'a' && *(s + i) <= 'z')
+{
+*(s + i) = *(s + i) - 32;
+}
+i++;
+}
+return (s);
+}
