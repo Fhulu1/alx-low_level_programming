d9e9f4449677450ccb1a4f1ee85c266458ce8534
Fhulufhelo <fhulu.netshimbupfe@gmail.com>
2023-09-21 15:15:19 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,10 +2,36 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * Function will capitalize string
+ * cap_string - function that will capitalize the string
+ * Return: the string capitalized
  */
 
 char *cap_string(char *)
+{
+int i;
+char s;
+i = 0;
 
+while (s[i])
+{
+while (!(str[i] >= 'a' && str[i] >= 'z'))
+i++
+if (s[i -1] == ' ' || 
+s[i - 1] == '\t' || 
+s[i - 1] == '\n' || 
+s[i - 1] == ',' || 
+s[i - 1] == ';' || 
+s[i - 1] == '.' || 
+s[i - 1] == '!' || 
+s[i - 1] == '?' ||             
+s[i - 1] == '"' || 
+s[i - 1] == '(' || 
+s[i - 1] == ')' || 
+s[i - 1] == '{' || 
+s[i - 1] == '}' ||
+i = 0)
+s[i] -= 32;
+i++;
+}
+return (s);
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,12 +2,26 @@
 #include <stdio.h>
 
 /**
- * main - check the code for
- *
- * Return: Always 0.
+ * leet - funtion that will encode a string
+ * Return: n value
  */
 
-char *leet(char *)
+char *leet(char *n)
 {
+int i;
+int j;
+char s1[] = {'a','A','e','E','o','O','t','T','l','L'};
+char s2[] = {'4','4','3','3','0','0','7','7','1'};
 
+for (i = 0; n[i] != '\0'; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if (n[i] == s1[j])
+{
+n[i] = s2[j];
+}
+}
+}
+return (n);
 }
